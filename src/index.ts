7542c624f821c3c99db1bc531e13c7374d141ae5
Narzullayev Shahriyor <narzullayevshaxriyor049@gmail.com>
2025-05-06 14:21:57 +0500

    Index update

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,19 @@ import blogRoutes from "./routes/blog.routes"
 import postRoutes from "./routes/post.routes"
 import commentRoutes from "./routes/comment.routes"
 
-// Load environment variables
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT 
 
-// Middlewares
 app.use(express.json())
 app.use(cookieParser())
 
-// Routes
 app.use("/api/auth", authRoutes)
 app.use("/api/blogs", blogRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/comments", commentRoutes)
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
